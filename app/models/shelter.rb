c4f2cc09c5b9b038e60bfeca0d53458be7da108a
username <wnt925@gmail.com>
2020-02-02 22:37:15 -0700

    Add shelters

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,6 @@
 class Shelter < ApplicationRecord
 
-validates :place_id, uniqueness: true
+# validates :place_id, uniqueness: true
 validates_presence_of :name, :address, :phone_number, :latitude, :longitude
 
 geocoded_by :zip
--- app/models/shelter_creator.rb
+++ app/models/shelter_creator.rb
@@ -9,16 +9,15 @@ class ShelterCreator
     lat = shelter_data[:geometry][:location][:lat]
     lng = shelter_data[:geometry][:location][:lng]
     place_id = shelter_data[:place_id]
-    Shelter.create!(name: name, address: address, phone_number: phone_number, hours: hours, latitude: lat, longitude: lng, zip: zip, place_id: place_id)
+    Shelter.create(name: name, address: address, phone_number: phone_number, hours: hours, latitude: lat, longitude: lng, zip: zip, place_id: place_id)
   end
 
   def populate_shelters(zip_codes)
     zip_codes.each do |zip|
       service = GoogleMapsService.new(zip, 16093.4)
       service.shelters_by_zip_code.map do |shelter_data|
-        create_record(shelter_data)
+       create_record(shelter_data)
       end
     end
   end
-
 end
